9847e122ee5395dfeb4020092bfc8e12c11c96e9
Filipe Fernandes <ocefpaf@gmail.com>
2020-06-04 15:28:52 -0300

    remove versioneer

--- mymodule/__init__.py
+++ mymodule/__init__.py
@@ -1,5 +1,7 @@
 from mymodule.mymodule import (meaning_of_life, meaning_of_life_url)
 __all__ = ["mymodule"]
-from ._version import get_versions
-__version__ = get_versions()["version"]
-del get_versions
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
