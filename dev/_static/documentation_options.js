d82d9e851103ba6b0e574095bb78a343e4befe9b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-12-22 23:03:01 +0000

    Update docs after building Travis build 35 of ioos/ioos-python-package-skeleton
    The docs were built from the branch 'master' against the commit
    7c41c8c77dc28e930476acb0ceab595d469e465f.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/ioos/ioos-python-package-skeleton/jobs/464119849.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.9/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key.enc --built-docs docs/build/html dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev4+g8c1a355',
+    VERSION: '1.0.1.dev7+g7c41c8c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
